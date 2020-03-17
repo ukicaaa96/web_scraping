ead6768d1676a0f3c8d4c22e8227de17774a0d24
Uros Jevtic <meksa96@gmail.com>
2020-03-17 16:09:31 +0100

    dodata klasa i filteri za cene

--- WebScraping_Kompjuteri.py
+++ WebScraping_Kompjuteri.py
@@ -1,69 +1,82 @@
-#Analiziramo sajt i trazimo potrebne informacije
+#Kupujem prodajem - Polovni racunari
 
 # class adPrice - cena
 # class adName - ime
-# class locationSc - mesto
+# class locationSec - mesto
 import bs4
 import requests
 
-cena = []
-cenaFinal =[]
+karakteri = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNMčČćĆšŠđĐžŽ- . ,0123456789,€dinKontakt Kupujem Pozvati Novo"
 
-naziv=[]
-nazivFinal=[]
+class Predmet:
+    def __init__(self, imeKlase, karakteriZaFilter,brojStrana):
+        self.imeKlase = imeKlase
+        self.karakteriZaFilter = karakteriZaFilter
+        self.brojStrana = brojStrana
+     
 
-mesto =[]
-mestoFinal = []
+    def Lista(self):#vraca listu (ime,cena,lokacija....)
+        lista = []
+        listaFinal =[]
+        for i in range(1,3):
+            url = "https://www.kupujemprodajem.com/Kompjuteri-Desktop/Polovni-kompjuteri/10-98-" + str(i)+"-grupa.htm"
+            res = requests.get(url)
+            soup = bs4.BeautifulSoup(res.text , "lxml")
+            for i in soup.select("."+self.imeKlase):
+                podatak = i.text
+                lista.append(str(podatak))
+            for j in lista:
+                if(j != "Mesto/Grad"):
+                    listaFinal.append(''.join(c for c in j if c in self.karakteriZaFilter))
+        return listaFinal
 
+def FilterCena(cenaLista):#bes decimalnih vrednosti samo cena i valuta
+    cenaFilter=[]
+    for i in cenaLista:
+        string = ""
+        prekidac = True
+        for j in i:
+            if (j != "," and prekidac == True):
+                string += j
+            elif(j == ","):
+                prekidac = False
+                
+        if("€" in i):
+            string += "€"
+            
+        cenaFilter.append(string)
+    return cenaFilter
+
+def FilterCenaINT(cenaLista):#pretvaramo cene u int
+    ceneINT = []
+    for i in cenaLista:
+        a = ''.join(c for c in i if c in "1234567890")
+        ceneINT.append(int(a))
+    print(ceneINT)
+    
+        
+
+brojStrana = 4# koliko strana zelimo da program preuzme
+
+p1 = Predmet("adName" , karakteri, brojStrana)
+p2 = Predmet("adPrice" , karakteri, brojStrana)
+p3 = Predmet("locationSec" , karakteri, brojStrana)
+
+ime = p1.Lista()
+cena = p2.Lista()
+mesto = p3.Lista()
+
+
+#recnik sa nazivom,cenom i lokacijom
 racunari = {}
 
-url = "https://www.kupujemprodajem.com/Kompjuteri-Desktop/Polovni-kompjuteri/10-98-1-grupa.htm"
-res = requests.get(url)
-soup = bs4.BeautifulSoup(res.text , "lxml")
+for i in range(1,len(p1.Lista())):
+    racunari.update({i:ime[i] + " | " + cena[i]+ " | " + mesto[i]})
 
-################################################################################
-for i in soup.select(".adPrice"):#Uzimamo svaki podatak iz klase adPrice - to je cena predemta
-    cene = i.text
-    cena.append(str(cene))#smestamo u listu koju cemo kasnije filtrirati
-for j in cena:# filtriranje - uzimamo samo brojeve i valutu bez ostalih stvari (/''{:#^ i nepotrebnih slova)
-    cenaFinal.append(''.join(c for c in j if c in '0123456789,€dinKontakt Kupujem Pozvati '))#praznom stringu dodajemo odgovarajuce karaktere
+for i in racunari:
+    print(racunari[i])
 
-################################################################################
 
-for i in soup.select(".adName"): #Uzimamo svaki podatak iz klase adName - to je ime predmeta
-    nazivi = i.text
-    naziv.append(str(nazivi))#smestamo u listu koju cemo kasnije filtrirati
-for j in naziv:# filtriranje - uzimamo samo slova bez ostalih stvari (/''{:$#^....)
-    nazivFinal.append(''.join(c for c in j if c in 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890 ')) #praznom stringu dodajemo odgovarajuce karaktere
 
-################################################################################
 
-for i in soup.select(".locationSec"): #Uzimamo svaki podatak iz klase locationSoc - to je lokacija oglasa
-    lokacije = i.text
-    mesto.append(str(lokacije))#smestamo u listu koju cemo kasnije filtrirati
-for j in mesto:# filtriranje - uzimamo samo slova bez ostalih stvari (/''{:$#^....)
-    if(j != "Mesto/Grad"):#Na sajtu je i naslov kolone gde se nalaze cene u istoj klasi kao cene tako da to ne zelimo da sacuvamo(preskacemo)
-        mestoFinal.append(''.join(c for c in j if c in 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890šŠđĐčČćĆŽž ')) #praznom stringu dodajemo odgovarajuce karaktere
-################################################################
-    
 
-for g in range (1,len(nazivFinal)): # formiramo recnik sa rednim brojevima
-    racunari.update({g:nazivFinal[g]+" : " + cenaFinal[g] + " : " + mestoFinal[g]})
-
-###############################################################################################
-while(True):
-    print("1.Ispisi svaki oglas kompjutera")
-    print("2.Ispisi samo oglase iz Beograda")
-    izbor = int(input(">"))
-
-    if(izbor == 1):#svi racunari sa prve stranice oglasa na KP 
-        for r in racunari:
-            print(racunari[r])    
-
-        
-    elif(izbor == 2):#samo racunari iz Beograda
-        for r in racunari:
-            if("Beograd" in racunari[r]):
-                print(racunari[r])
-
-    print("-------------------------------------------")
